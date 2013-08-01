50a51aaafbf65db9f8e8113c4111c5dcf709910a
Pat Jameson <patrickejameson@gmail.com>
2013-08-01 13:12:03 -0700

    Indentation fix

--- js/datatable-highlight.js
+++ js/datatable-highlight.js
@@ -1,21 +1,21 @@
 function datatableHighlight() {}
 
 datatableHighlight.ATTRS = {
-	highlightRows: {
-		value: false,
-		setter: '_highlightRows',
-		validator: Y.Lang.isBoolean
-	},
-	highlightCols: {
-		value: false,
-		setter: '_highlightCols',
-		validator: Y.Lang.isBoolean
-	},
-	highlightCells: {
-		value: false,
-		setter: '_highlightCells',
-		validator: Y.Lang.isBoolean
-	}
+    highlightRows: {
+        value: false,
+        setter: '_setHighlightableRows',
+        validator: Y.Lang.isBoolean
+    },
+    highlightCols: {
+        value: false,
+        setter: '_setHighlightableCols',
+        validator: Y.Lang.isBoolean
+    },
+    highlightCells: {
+        value: false,
+        setter: '_setHighlightableCells',
+        validator: Y.Lang.isBoolean
+    }
 };
 
 
@@ -29,25 +29,25 @@ datatableHighlight.prototype = {
    
     _colNameRegex: /yui3-datatable-col-(\S*)/,
    
-	_currentRowsDelegate: null,
+    _currentRowsDelegate: null,
     _currentColsDelegate: null,
     _currentCellsDelegate: null,
 
-	_highlightRows: function (highlightingRows) {
-		if (highlightingRows) {
+    _setHighlightableRows: function (highlightingRows) {
+        if (highlightingRows) {
             if (this._currentRowsDelegate) {
                 this._currentRowsDelegate.detach();
             }
             
-			this._currentRowsDelegate = this.delegate('hover', 
+            this._currentRowsDelegate = this.delegate('hover', 
                 Y.bind(this._highlightRowsOn, this), 
                 Y.bind(this._highlightRowsOff, this),
             "tr");
-		}
-	},
+        }
+    },
 
-	_highlightCols: function (highlightingCols) {
-		if (highlightingCols) {
+    _setHighlightableCols: function (highlightingCols) {
+        if (highlightingCols) {
             if (this._currentColsDelegate) {
                 this._currentColsDelegate.detach();
             }
@@ -56,28 +56,28 @@ datatableHighlight.prototype = {
                 Y.bind(this._highlightColsOn, this),
                 Y.bind(this._highlightColsOff, this),
             "tr td");
-		}
-	},
+        }
+    },
 
-	_highlightCells: function (highlightingCells) {
-		if (highlightingCells) {
+    _setHighlightableCells: function (highlightingCells) {
+        if (highlightingCells) {
             if (this._currentCellsDelegate) {
                 this._currentCellsDelegate.detach();
             }
 
-			this._currentCellsDelegate = this.delegate('hover', 
+            this._currentCellsDelegate = this.delegate('hover', 
                 Y.bind(this._highlightCellsOn, this), 
                 Y.bind(this._highlightCellsOff, this),
             "tr td");
-		}
-	},
+        }
+    },
 
     _highlightCellsOn: function(e) {
         e.currentTarget.addClass(this._cellsHighlightedCSS);
     },
     
     _highlightCellsOff: function(e) {
-	    e.currentTarget.removeClass(this._cellsHighlightedCSS);
+        e.currentTarget.removeClass(this._cellsHighlightedCSS);
     },
 
     _highlightColsOn: function(e) {
